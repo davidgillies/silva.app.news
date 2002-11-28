b68a9d5413f9cca0db9a016dfec26dd5b3f6d099
Guido Wesdorp <info@infrae.com>
2002-11-28 14:28:17 +0000

    Fixed typo

--- install.py
+++ install.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.9 $
+# $Revision: 1.10 $
 
-"""Install and Uninstall for Silva Eur
+"""Install and Uninstall for Silva News
 """
 
 from Products.FileSystemSite.DirectoryView import manage_addDirectoryView
