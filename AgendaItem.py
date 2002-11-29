0577bac48a7b98bf1c2e8d115682dac3674bad4b
Guido Wesdorp <info@infrae.com>
2002-11-29 09:59:40 +0000

    Fixed small bug: fulltext search was broken, since it still used the old _location attribute

--- AgendaItem.py
+++ AgendaItem.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.9 $
+# $Revision: 1.10 $
 
 # Zope
 from AccessControl import ClassSecurityInfo
@@ -83,7 +83,7 @@ class AgendaItemVersion(NewsItemVersion):
         """Deliver the contents as plain text, for full-text search
         """
         parenttext = AgendaItemVersion.inheritedAttribute('fulltext')(self)
-        return "%s %s %s" % (parenttext, self._location, self._location_manual)
+        return "%s %s" % (parenttext, self._location_manual)
 
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
                               'to_xml')
