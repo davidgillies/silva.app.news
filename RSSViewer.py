bd77d231cf341d9a04eea5c41a4a5c1ee8f01d93
Guido Wesdorp <info@infrae.com>
2003-01-27 18:37:10 +0000

    Added some caching functionality, and also a timeout. The timeout is rather nasty: since Python has no way (built-in, and afaik) to set timeout for HTTP operations, I abuse the threading interface to create some sort of timeout situation. This is also done in this way in Formulator, and seems to work okay... Caching is now done in 2 different manners: first I added a setting which can be modified on the viewers that will make the system not check for new data for a certain period, whatever happens, and the second thing I do now is check whether the data received is newer than the current cached data, and if not, don't parse it.

--- RSSViewer.py
+++ RSSViewer.py
@@ -1,14 +1,17 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 
 from urllib import urlopen
+import time
+from threading import Thread
 from xml.dom.minidom import parseString
 
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from OFS import Folder
+from DateTime import DateTime
 
 from Products.Silva.IContent import IContent
 from Products.Silva.TocSupport import TocSupport
@@ -19,6 +22,67 @@ from Products.Silva.helpers import add_and_edit
 
 from Products.SilvaNews.NewsViewer import NewsViewer
 
+class TimeoutWrapper:
+    """Used to run a certain method with a timeout.
+
+    Usage:
+
+    t = TimeoutWrapper(<method>, <args>, <kwargs>)
+    success = t.run_with_timeout(<timeout>)
+    if success:
+        retval = t.return_value()
+    else:
+        raise Exception, 'Operation timed out'
+
+    When calling the 'run_with_timeout' method, <method> is called with arguments <args> and keyword arguments <kwargs>.
+    If the method succeeds within the requested time, 1 is returned, and the result of the call can be retrieved using 
+    the 'return_value' method. If this process takes longer then <timeout> (seconds), 0 is returned by the 
+    'run_with_timeout' method (and 'return_value' returns None).
+    """
+    
+    def __init__(self, handler, args=(), kwargs={}):
+        """Initializes the object.
+        
+        Arguments are:
+        
+        - handler: a reference to the method to be called
+        - args: a list of non-keyword arguments that should be passed to the method
+        - kwargs: a dict of keyword arguments that should be passed to the method
+        """
+        self._handler = handler
+        self._args = args
+        self._kwargs = kwargs
+        self._success = 0
+        self._retval = None
+
+    def run_with_timeout(self, timeout):
+        """Class the method
+
+        Arguments are:
+        
+        - timeout: an integer (or float) specifying the timeout in seconds
+        """
+        thread = Thread(target=self._run_thread)
+        thread.start()
+        thread.join(timeout)
+        del thread
+        return self._success
+
+    def return_value(self):
+        """Returns the return value of the method (if any)
+
+        No arguments required.
+        """
+        return self._retval
+
+    def _run_thread(self):
+        """Internal method that actually calls the method.
+
+        Will set self._retval and self._success if successful.
+        """
+        self._retval = self._handler(*self._args, **self._kwargs)
+        self._success = 1
+        
 def get_text_from_children(node):
     """Returns all textnode values concatenated"""
     retval = ''
@@ -68,12 +132,85 @@ class RSSViewer(NewsViewer):
     def __init__(self, id, title):
         RSSViewer.inheritedAttribute('__init__')(self, id, title)
         self._rss_feed = ''
+        self._rss_last_modified = ''
+        self._last_rss_feed = ''
+        self._last_result = []
+        self._caching_period = 30 # in seconds, 0 means don't cache
+        self._last_request_time = time.time()
+        self._rss_timeout = 30 # in seconds
 
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
                               'get_items')
     def get_items(self):
+        """Gets the items from the RSS server and serves them
+        
+        Will try to cache in 2 different ways:
+        
+        1. Using a time setting - the manager of the RSS viewer can set a minimal amount of time the content should be cached,
+                                            before this time is due the viewer will use cached data (if any), regardless of whether there
+                                            is new data available on the server
+        2. Using HTTP caching headers - if the server sends out an HTTP Last-Modified header, this is checked against the last 
+                                            received Last-Modified header, and if those two are the same then the viewer will use
+                                            cached data (if any). FIXME: It would be nice to just send a HEAD request instead of the full monty...
+        """
+        result = []
+        # check if we want to use the cached version because of time settings, if so:
+        if (self._last_result and 
+            self._rss_feed == self._last_rss_feed and 
+            self._caching_period != 0 and 
+            (time.time() - self._last_request_time) < self._caching_period):
+            print "Using cached version because of timeout settings"
+            # use the cached version
+            result = self._last_result
+            # update the timestamps
+            self._last_request_time = time.time()
+        # if we're not gonna use the cached version because of caching_period settings:
+        else:
+            # download the data (with a timeout)
+            t = TimeoutWrapper(urlopen, (self._rss_feed,))
+            success = t.run_with_timeout(self._rss_timeout)
+            # if successful:
+            if success:
+                # get the headers and data
+                ret = t.return_value()
+                info = ret.info()
+                data = ret.read()
+                # check if there is a new version (using Last-Modified), if there isn't:
+                lm = info.getheader('Last-Modified', None)
+                if lm and lm == self._rss_last_modified:
+                    print "Using cached version because of Last-Modified header"
+                    # use the cached version
+                    result = self._last_result
+                    # update the timestamps
+                    self._last_request_time = time.time()
+                # if there is a new version or the Last-Modified header is not set:
+                else:
+                    print "Headers:"
+                    print info.headers
+                    print "Retrieving new data"
+                    # parse the new version
+                    result = self._parse_rss_stream(data)
+                    # update the caches
+                    self._last_result = result
+                    self._last_rss_feed = self._rss_feed
+                    # update the timestamps
+                    self._last_request_time = time.time()
+            # else if not successful:
+            else:
+                print "Using cached version because of timeout"
+                # use the cached version
+                result = self._last_result
+                # update the timestamps
+                self._last_request_time = time.time()
+
+        self._p_changed = 1
+        # return
+        return result
+
+    def _parse_rss_stream(self, xml):
         """Gets the items from the RSS feed
         """
+        results = []
         # I declare properties here, since they need to be flushed anyway
         self._rss_title = ''
         self._rss_link = ''
@@ -87,9 +224,9 @@ class RSSViewer(NewsViewer):
         self._rss_textinput_description = ''
         self._rss_textinput_name = ''
         self._rss_textinput_link = ''
-        results = []
-        feedxml = urlopen(self._rss_feed).read()
-        dom = parseString(feedxml)
+        self._last_rss_feed = self._rss_feed
+
+        dom = parseString(xml)
         rssnode = None
         for node in dom.childNodes:
             if node.nodeName == u'rss' or node.nodeName == u'rdf:RDF':
