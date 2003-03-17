2b3c1410bd242cf3aa0c23d1a74ea7744c5d0c16
Guido Wesdorp <info@infrae.com>
2003-03-17 17:16:45 +0000

    Fixed bug where the container of newsitems got uncataloged as soon as the item itself got uncataloged (double uncataloging)

--- NewsItem.py
+++ NewsItem.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.20 $
+# $Revision: 1.21 $
 
 # Python
 from StringIO import StringIO
@@ -54,7 +54,6 @@ class NewsItem(CatalogedVersionedContent):
         NewsItem.inheritedAttribute('manage_beforeDelete')(
             self, item, container)
         container._remove_ordered_id(item)
-        self.unindex_object()
 
     # MANIPULATORS
     # ACCESSORS
