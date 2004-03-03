0c16838d460025c459ad1ca6b7f8c2de07393307
Guido Wesdorp <info@infrae.com>
2004-03-03 15:57:11 +0000

    Fixed bug that made AgendaItems shown in NewsFilters even though it was told not to show them.

--- NewsFilter.py
+++ NewsFilter.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.24 $
+# $Revision: 1.25 $
 
 from OFS import SimpleItem
 from AccessControl import ClassSecurityInfo
@@ -335,11 +335,11 @@ class NewsFilter(Filter):
         mts = self.get_root().filtered_meta_types()
         for mt in mts:
             if mt.has_key('instance'):
-                if INewsItemVersion.isImplementedByInstancesOf(mt['instance']):
+                if ((self._show_agenda_items and 
+                            IAgendaItemVersion.isImplementedByInstancesOf(mt['instance'])) 
+                        or (INewsItemVersion.isImplementedByInstancesOf(mt['instance']) and not
+                            IAgendaItemVersion.isImplementedByInstancesOf(mt['instance']))):
                     allowed.append(mt['name'])
-                elif self._show_agenda_items:
-                    if IAgendaItemVersion.isImplementedByInstancesOf(mt['instance']):
-                        allowed.append(mt['name'])
         return allowed
 
     security.declarePublic('rss_feed')
