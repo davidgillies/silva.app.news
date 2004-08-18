1ac965160d5b350ce595da3c32decb1e60ec6bde
Guido Wesdorp <info@infrae.com>
2004-08-18 10:14:07 +0000

    Added missing 'object.set_title(title)' call.

--- NewsPublication.py
+++ NewsPublication.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 
 # Zope
 from AccessControl import ClassSecurityInfo
@@ -144,5 +144,6 @@ def manage_addNewsPublication(self, id, title, create_default=1, REQUEST=None):
     object = NewsPublication(id)
     self._setObject(id, object)
     object = getattr(self, id)
+    object.set_title(title)
     add_and_edit(self, id, REQUEST)
     return ''
