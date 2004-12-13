e195c18cebbfe408c1c33d3d5f3b055ce6ba25d5
Infrae <info@infrae.com>
2004-12-13 09:18:36 +0000

    Fixed dynamic months i18n in get_month, now uses silva_news domain. Fixed view_archive templates. More translations...

--- views/get_months.py
+++ views/get_months.py
@@ -12,17 +12,17 @@
 from Products.Silva.i18n import translate as _
 
 
-months = [ _('Jan'),
-           _('Feb'),
-           _('Mar'),
-           _('Apr'),
-           _('May'),
-           _('Jun'),
-           _('Jul'),
-           _('Aug'),
-           _('Sep'),
-           _('Oct'),
-           _('Nov'),
-           _('Dec') ]
+months = [ _('Jan','silva_news'),
+           _('Feb','silva_news'),
+           _('Mar','silva_news'),
+           _('Apr','silva_news'),
+           _('May','silva_news'),
+           _('Jun','silva_news'),
+           _('Jul','silva_news'),
+           _('Aug','silva_news'),
+           _('Sep','silva_news'),
+           _('Oct','silva_news'),
+           _('Nov','silva_news'),
+           _('Dec','silva_news') ]
 
 return [ unicode(m) for m in months ]
