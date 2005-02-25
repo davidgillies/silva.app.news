cd0d583e6b8ef11d7ba1dd0b81fd0ca25a3474ee
Guido Wesdorp <info@infrae.com>
2005-02-25 11:51:16 +0000

    Fixed small bug that didn't unregister the right views on uninstall.

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.24 $
+# $Revision: 1.25 $
 
 """Install and Uninstall for Silva News
 """
@@ -114,8 +114,8 @@ def unregisterViews(reg):
     reg.unregister('public', 'Silva News Viewer')
     reg.unregister('public', 'Silva RSS Aggregator')
     reg.unregister('public', 'Silva Agenda Viewer')
-    reg.unregister('public', 'Silva Article')
-    reg.unregister('public', 'Silva Agenda Item')
+    reg.unregister('public', 'Silva Article Version')
+    reg.unregister('public', 'Silva Agenda Item Version')
     # add
     reg.unregister('add', 'Silva Agenda Filter')
     reg.unregister('add', 'Silva News Filter')
