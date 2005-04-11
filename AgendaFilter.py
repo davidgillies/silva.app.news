09c07e2db861d36630232e396b060115d956bc21
Guido Wesdorp <info@infrae.com>
2005-04-11 15:14:37 +0000

    Changed ordering, rather than by end date/time it now orders by start date/time.

--- AgendaFilter.py
+++ AgendaFilter.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.23 $
+# $Revision: 1.24 $
 
 from OFS import SimpleItem
 from AccessControl import ClassSecurityInfo
@@ -22,13 +22,9 @@ addable_priority = 3.4
 
 def brainsorter(a, b):
     aobj = a.getObject()
-    atime = aobj.end_datetime()
-    if atime is None:
-        atime = aobj.start_datetime()
+    atime = aobj.start_datetime()
     bobj = b.getObject()
-    btime = bobj.end_datetime()
-    if btime is None:
-        btime = bobj.start_datetime()
+    btime = bobj.start_datetime()
     return cmp(atime, btime)
 
 class AgendaFilter(Filter):
