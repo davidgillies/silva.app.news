927f16f626fcb27e1cc115873f8b90dc78cf93bc
Guido Wesdorp <info@infrae.com>
2005-04-11 14:59:41 +0000

    Re-added some scripts which I had accidentally removed (seems they're cursed or so, second time with the same scripts ;).

---
+++ views/edit/VersionedContent/NewsItem/tab_status_approve.py
@@ -0,0 +1,66 @@
+from Products.Silva.i18n import translate as _
+from Products.Formulator.Errors import ValidationError, FormValidationError
+
+model = context.REQUEST.model
+view = context
+
+if not model.get_unapproved_version():
+    # SHORTCUT: To allow approval of closed docs with no new version available,
+    # first create a new version. This "shortcuts" the workflow.
+    # See also edit/Container/tab_status_approve.py
+    if model.is_version_published():
+        return view.tab_status(
+            message_type="error", 
+            message=_("There is no unapproved version to approve."))
+    model.create_copy()
+
+try:
+    result = view.tab_status_form_editor.validate_all(context.REQUEST)
+except FormValidationError, e:
+    return view.tab_status(
+        message_type="error", message=view.render_form_errors(e))
+
+try:
+    result_news_form = view.tab_status_form_news.validate_all(context.REQUEST)
+except FormValidationError, e:
+    # highly unlikely
+    return view.tab_status(message_type='error', 
+                                message='Error in display datetime fields')
+
+import DateTime
+
+unapproved = getattr(model, model.get_unapproved_version())
+if (hasattr(unapproved, 'implements_newsitem') and 
+        unapproved.implements_newsitem()):
+    # set display datetime if required
+    pdt = result['publish_datetime']
+    if result['publish_now_flag']:
+        pdt = DateTime.DateTime()
+    if (result_news_form.has_key('update_display_datetime') and 
+            result_news_form['update_display_datetime']):
+        ddt = result_news_form['display_datetime']
+        if ddt is None:
+            ddt = pdt
+        unapproved.set_display_datetime(ddt)
+    elif unapproved.display_datetime() is None:
+        # there's no initial display datetime yet, let's set it
+        unapproved.set_display_datetime(pdt)
+else:
+    raise 'not a news item'
+    
+if result['publish_now_flag']:
+    model.set_unapproved_version_publication_datetime(DateTime.DateTime())
+else:
+    model.set_unapproved_version_publication_datetime(
+                                        result['publish_datetime'])
+
+expiration = result['expiration_datetime']
+if expiration:
+    model.set_unapproved_version_expiration_datetime(expiration)
+
+model.approve_version()
+
+if hasattr(model, 'service_messages'):
+    model.service_messages.send_pending_messages()
+    
+return view.tab_status(message_type="feedback", message=_("Version approved."))
