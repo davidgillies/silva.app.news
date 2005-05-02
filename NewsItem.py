1ddab1616e27b727911bca32d1aa59241f5b367f
Guido Wesdorp <info@infrae.com>
2005-05-02 15:02:17 +0000

    Fixed bug with description fetching from meta data (wrong field id).

--- NewsItem.py
+++ NewsItem.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.29 $
+# $Revision: 1.30 $
 
 # Python
 from StringIO import StringIO
@@ -178,7 +178,7 @@ class NewsItemVersion(DocumentVersion):
                                 'get_description')
     def get_description(self):
         binding = self.service_metadata.getMetadata(self)
-        desc = binding.get('silva-extra', 'description')
+        desc = binding.get('silva-extra', 'content_description')
         return desc
         
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
