5b541b228586cb69fb0fb07223d298ba8aff6fad
Wim Boucquaert <wim.boucquaert@gmail.com>
2006-07-18 15:37:28 +0000

    think I fixed the +response = request.RESPONSE

--- views/edit/VersionedContent/NewsItem/kupu_content.py
+++ views/edit/VersionedContent/NewsItem/kupu_content.py
@@ -28,8 +28,26 @@ def entitize_and_escape_pipes(str):
     str = str.replace('|', '&pipe;')
     return str
 
-context.set_cache_headers()
-context.set_content_type()
+response = request.RESPONSE
+headers = [('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT'),
+            ('Last-Modified', 
+                DateTime("GMT").strftime("%a, %d %b %Y %H:%M:%S GMT")),
+            ('Cache-Control', 'no-cache, must-revalidate'),
+            ('Cache-Control', 'post-check=0, pre-check=0'),
+            ('Pragma', 'no-cache'),
+            ]
+
+placed = []
+for key, value in headers:
+    if key not in placed:
+        response.setHeader(key, value)
+        placed.append(key)
+    else:
+        response.addHeader(key, value)
+
+
+response.setHeader('Content-Type', 'text/html;charset=utf-8')
+
 docref = model.create_ref(model)
 doctitle = version.get_title()
 xhtml = version.content.editorHTML()
