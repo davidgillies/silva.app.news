18e473ee3a13f1808a6df1af3b31198fb268fcf4
Wim Boucquaert <wim.boucquaert@gmail.com>
2006-07-26 11:16:10 +0000

    improved usability and some compliancy

--- views/edit/Container/NewsPublication/settings_submit.py
+++ views/edit/Container/NewsPublication/settings_submit.py
@@ -20,11 +20,16 @@ message_type = 'error'
 current_private = model.is_private()
 is_private = request.has_key('is_private')
 
+if is_private:
+    new_status = 'localy'
+else:
+    new_status = 'globaly'
+
 if is_private != current_private:
     model.set_private(is_private)
-    m = _('Settings changed.', 'silva_news')
+    m = _('The news source is now ' + new_status + ' available.', 'silva_news')
     message = unicode(m)
     message_type = 'feedback'
     model.sec_update_last_author_info()
 
-return view.tab_edit(message_type=message_type, message=message)
+return view.tab_settings(message_type=message_type, message=message)
