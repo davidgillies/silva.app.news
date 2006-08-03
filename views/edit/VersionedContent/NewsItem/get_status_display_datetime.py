74d45540f41fe91ee3742847de465444bcb4a594
Wim Boucquaert <wim.boucquaert@gmail.com>
2006-08-03 15:57:55 +0000

    fixed issue 1603 tnx@martijn

---
+++ views/edit/VersionedContent/NewsItem/get_status_display_datetime.py
@@ -0,0 +1,18 @@
+##parameters=model
+
+editable = model.get_editable()
+if editable is None:
+    viewable = model.get_viewable()
+    if viewable is not None:
+        return None
+    last_closed = model.get_last_closed()
+    if last_closed is None:
+        # probably this case never occurs as there's always
+        # at least a last_closed version if there's no editable and
+        # no viewable version
+        return None
+    return last_closed.display_datetime()
+return editable.display_datetime()
+
+
+
