d0d267aae873549d7a9b6cf3d254e094d18f3615
Wim Boucquaert <wim.boucquaert@gmail.com>
2006-08-03 16:22:35 +0000

    fixed error that if a version was not unapproved ...

--- views/edit/Container/NewsPublication/tab_status_open_now.py
+++ views/edit/Container/NewsPublication/tab_status_open_now.py
@@ -20,12 +20,14 @@ clear_expiration = result['clear_expiration']
 
 objects = [model.resolve_ref(ref) for ref in refs]
 
-
 # set the display_date_time
 now = DateTime()
 for obj in objects:
     if hasattr(obj, 'implements_newsitem') and obj.implements_newsitem():
-        unapproved = getattr(obj, obj.get_unapproved_version())
+        unapproved = obj.get_unapproved_version()
+        if unapproved is None:
+            continue
+        unapproved = getattr(obj, unapproved)
         if unapproved.display_datetime() is None:
             unapproved.set_display_datetime(now)
 
