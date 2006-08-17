c67102539b8e5f1c1814c6f21652079cc40488d3
Wim Boucquaert <wim.boucquaert@gmail.com>
2006-08-17 12:20:08 +0000

    fixes for rendering the correct display date in multiple places

--- views/edit/VersionedContent/NewsItem/get_status_display_datetime.py
+++ views/edit/VersionedContent/NewsItem/get_status_display_datetime.py
@@ -1,20 +1,21 @@
 ##parameters=model
 
-editable = model.get_editable()
+next_id = model.get_next_version()
+if next_id is not None:
+    next = getattr(model, next_id)
+    return next.display_datetime()
+    
+viewable = model.get_viewable()
+if viewable is not None:
+    return None
+last_closed = model.get_last_closed()
+if last_closed is None:
+    # probably this case never occurs as there's always
+    # at least a last_closed version if there's no editable and
+    # no viewable version
+    return None
+return last_closed.display_datetime()
 
-if editable is None:
-    viewable = model.get_viewable()
-    approved = model.get_approved_version()
-    if viewable is not None:
-        return None
-    last_closed = model.get_last_closed()
-    if last_closed is None:
-        # probably this case never occurs as there's always
-        # at least a last_closed version if there's no editable and
-        # no viewable version
-        return None
-    return last_closed.display_datetime()
-return editable.display_datetime()
 
 
 
