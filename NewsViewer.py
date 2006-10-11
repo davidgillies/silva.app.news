18af368603bbd1f72dd21143c424298073edbe24
Eric Casteleijn <ec@trapit.com>
2006-10-11 16:37:56 +0000

    added get_intro to rss feeds

--- NewsViewer.py
+++ NewsViewer.py
@@ -301,7 +301,7 @@ class NewsViewer(Content, Folder.Folder):
         xml.write('<title>%s</title>\n' % quote_xml(item.get_title()))
         xml.write('<link>%s</link>\n' % quote_xml(version_container.absolute_url()))
         xml.write('<description>%s</description>\n' %
-                  quote_xml(mdbinding.get('silva-extra', 'content_description')))
+                  quote_xml(item.get_intro())
         # DC elements
         xml.write('<dc:subject>%s</dc:subject>\n' %
                   quote_xml(mdbinding.get('silva-extra', 'subject')))
