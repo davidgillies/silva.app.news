c3a41aa42f69880b5c8c5141503184d271f5ccf2
Andy Altepeter <aaltepet@altepeter.net>
2008-08-13 02:42:38 +0000

    removed unused rss.xml/layout area, since this is now handled through z3 views

--- layout/rss.xml.py
+++
@@ -1,6 +0,0 @@
-if not hasattr(context.aq_explicit, 'rss'):
-    raise 'NotFound', 'This object does not support RSS export'
-
-context.REQUEST.RESPONSE.setHeader('Content-Type', 'text/xml;charset=UTF-8')
-
-return context.rss()
