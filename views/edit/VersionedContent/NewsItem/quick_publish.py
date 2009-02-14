28292a102dadda1226d20568ab0d682744c9b087
Andy Altepeter <aaltepet@altepeter.net>
2009-02-14 02:41:41 +0000

    fixup 'publish now' so display datetime is actually set

--- views/edit/VersionedContent/NewsItem/quick_publish.py
+++ views/edit/VersionedContent/NewsItem/quick_publish.py
@@ -2,26 +2,30 @@ from Products.Silva.i18n import translate as _
 model = context.REQUEST.model
 view = context
 
+#get the tab to return to, defaulting to tab_edit
+return_to_id = context.REQUEST.get('return_to','tab_edit')
+return_to = getattr(view,return_to_id,'tab_edit')
+
 if not model.get_unapproved_version():
     # SHORTCUT: To allow approval of closed docs with no new version available,
     # first create a new version. This "shortcuts" the workflow.
     # See also edit/Container/tab_status_approve.py
     if model.is_version_published():
-        return view.tab_edit(
+        return return_to(
             message_type="error", 
             message=_("There is no unapproved version to approve."))
     model.create_copy()
 
 import DateTime
-
 now = DateTime.DateTime()
-editable = model.get_editable()
 model.set_unapproved_version_publication_datetime(now)
+editable = model.get_editable()
 if editable.display_datetime() is None:
     editable.set_display_datetime(now)
 model.approve_version()
 
 if hasattr(model, 'service_messages'):
     model.service_messages.send_pending_messages()
-    
-return view.tab_metadata(message_type="feedback", message=_("Version approved."))
+
+
+return return_to(message_type="feedback", message=_("Version approved."))
