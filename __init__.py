8b84f2d844841760ebd4eb1ecdbf6df0615cf5b5
Antonin Amand <antonin.amand@gmail.com>
2010-05-10 16:28:46 +0000

    add upgrade classes renaming

--- __init__.py
+++ __init__.py
@@ -27,3 +27,29 @@ def __allow_access_to_unprotected_subobjects__(name, value=None):
     return name in ('dates',)
 
 
+CLASS_CHANGES = {
+    'Products.SilvaNews.silvaxmlattribute.SilvaXMLAttribute':
+        'Persitence.Persistent',
+
+    # filters
+
+    'Products.SilvaNews.AgendaFilter.AgendaFilter':
+        'Products.SilvaNews.filters.AgendaFilter.AgendaFilter',
+    'Products.SilvaNews.CategoryFilter.CategoryFilter':
+        'Products.SilvaNews.filters.CategoryFilter.CategoryFilter',
+    'Products.SilvaNews.Filter.Filter':
+        'Products.SilvaNews.filters.Filter.Filter',
+    'Products.SilvaNews.NewsFilter.NewsFilter':
+        'Products.SilvaNews.filters.NewsFilter.NewsFilter',
+    'Products.SilvaNews.NewsItemFilter.NewsItemFilter':
+        'Products.SilvaNews.filters.NewsItemFilter.NewsItemFilter',
+
+    # viewers
+
+    'Products.SilvaNews.AgendaViewer.AgendaViewer':
+        'Products.SilvaNews.viewers..AgendaViewer.AgendaViewer',
+    'Products.SilvaNews.InlineViewer.InlineViewer':
+        'Products.SilvaNews.viewers.InlineViewer.InlineViewer',
+    'Products.SilvaNews.NewsViewer.NewsViewer':
+        'Products.SilvaNews.viewers.NewsViewer.NewsViewer',
+}
