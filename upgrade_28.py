d83174fb74a3f6a73fd2b462d5d7b266a5b32626
Sylvain Viollon <thefunny@gmail.com>
2010-06-21 16:01:20 +0000

    Extensions are refresh after an upgrade. Doing an upgrader to refresh of a extension just breaks everything.

--- upgrade_28.py
+++ upgrade_28.py
@@ -1,10 +1,12 @@
 # Copyright (c) 2002-2009 Infrae. All rights reserved.
 # See also LICENSE.txt
 # $Id$
+
 from silva.core.upgrade.upgrade import BaseUpgrader
 
 #this is the Silva version these upgrades should run under
 VERSION_B1='2.2b1'
+
 
 class IndexUpgrader(BaseUpgrader):
     """ remove the idx_is_private catalog index; it's
@@ -15,30 +17,9 @@ class IndexUpgrader(BaseUpgrader):
             obj.service_catalog.delIndex('idx_is_private')
         return obj
 
+
 indexupgrader = IndexUpgrader(VERSION_B1, 'Silva Root')
 
-class SNNRefresher(BaseUpgrader):
-    """ Root upgrader to refresh SNN if it was
-        installed in Silva 2.1.  Since the extension
-        registry is installed test has changed, this
-        needs to check if the SilvaNews view directory
-        is present.  If it is, install the extension
-        """
-    
-    def upgrade(self, obj):
-        se = obj.service_extensions
-        name = 'SilvaNews'
-        if hasattr(obj.service_views.aq_explicit, name):
-            #SNN was installed, so reinstall
-            if se.is_installed(name):
-                se.refresh(name)
-            else:
-                se.install(name)
-        return obj
-    
-#give it the highest priority so that the silva core 
-# metadata and secondrootupgraders can run first
-SNNRefresher = SNNRefresher(VERSION_B1, 'Silva Root', 0)
 
 class NewsPubIsPrivateUpgrader(BaseUpgrader):
     """ upgrade obj._is_private to snn-np-settings: is_private
@@ -58,14 +39,16 @@ class NewsPubIsPrivateUpgrader(BaseUpgrader):
 newspubupgrader = NewsPubIsPrivateUpgrader(
     VERSION_B1, 'Silva News Publication', 100)
 
+
 class SNNServiceCleanup(BaseUpgrader):
     """ this upgrader cleans up the service news, as it no longer
         manages SNN's upgrades """
-    
+
     def upgrade(self, obj):
         #remove service_news._content_version
         if hasattr(obj.aq_explicit, '_content_version'):
             del obj._content_version
         return obj
 
-SNNServiceCleanupUpgrader = SNNServiceCleanup(VERSION_B1, 'Silva News Service', 50)
+SNNServiceCleanupUpgrader = SNNServiceCleanup(
+    VERSION_B1, 'Silva News Service', 50)
