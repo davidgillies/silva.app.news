648c09328e04e3184f52d6f0be74b8706d2b40ed
Sylvain Viollon <thefunny@gmail.com>
2010-06-24 14:27:51 +0000

    Fix upgraders.

--- upgrade_28.py
+++ upgrade_28.py
@@ -18,7 +18,31 @@ class IndexUpgrader(BaseUpgrader):
         return obj
 
 
-indexupgrader = IndexUpgrader(VERSION_B1, 'Silva Root')
+index_upgrader = IndexUpgrader(VERSION_B1, 'Silva Root')
+
+
+class SNNRefresher(BaseUpgrader):
+    """ Root upgrader to refresh SNN if it was
+        installed in Silva 2.1.  Since the extension
+        registry is installed test has changed, this
+        needs to check if the SilvaNews view directory
+        is present.  If it is, install the extension
+        """
+
+    def upgrade(self, obj):
+        se = obj.service_extensions
+        name = 'SilvaNews'
+        if hasattr(obj.service_views.aq_explicit, name):
+            #SNN was installed, so reinstall
+            if se.is_installed(name):
+                se.refresh(name)
+            else:
+                se.install(name)
+        return obj
+
+# give it the highest priority so that the silva core
+# metadata and secondrootupgraders can run first
+snn_refresher = SNNRefresher(VERSION_B1, 'Silva Root', 1000)
 
 
 class NewsPubIsPrivateUpgrader(BaseUpgrader):
@@ -36,7 +60,7 @@ class NewsPubIsPrivateUpgrader(BaseUpgrader):
             binding.setValues('snn-np-settings',{'is_private': newip})
         return obj
 
-newspubupgrader = NewsPubIsPrivateUpgrader(
+news_pub_upgrader = NewsPubIsPrivateUpgrader(
     VERSION_B1, 'Silva News Publication', 100)
 
 
@@ -50,5 +74,5 @@ class SNNServiceCleanup(BaseUpgrader):
             del obj._content_version
         return obj
 
-SNNServiceCleanupUpgrader = SNNServiceCleanup(
+snn_service_cleanup_upgrader = SNNServiceCleanup(
     VERSION_B1, 'Silva News Service', 50)
