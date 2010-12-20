820acedbe2f1138bfdab87623c57f29da8d3564b
Sylvain Viollon <thefunny@gmail.com>
2010-12-20 15:36:02 +0100

    Tag 2.9.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.9dev'
+version = '2.9'
 
 def product_readme(filename):
     return  open(os.path.join('Products', 'SilvaNews', filename)).read()
