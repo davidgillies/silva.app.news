0932ac627a98e4575d526ee8a3ea1284004863ad
Sylvain Viollon <thefunny@gmail.com>
2011-03-25 17:18:37 +0100

    Comment out broken code.

--- Products/SilvaNews/adapters/feedentry.py
+++ Products/SilvaNews/adapters/feedentry.py
@@ -2,34 +2,35 @@
 # See also LICENSE.txt
 # $Id$
 
-from five import grok
-from Products.SilvaDocument.adapters import feedentry
-from Products.SilvaNews.interfaces import INewsItem, IAgendaItem
+# XXX Need fixes as SilvaDocument is gone
+# from five import grok
+# from Products.SilvaDocument.adapters import feedentry
+# from Products.SilvaNews.interfaces import INewsItem, IAgendaItem
 
 
-class NewsItemFeedEntryAdapter(feedentry.DocumentFeedEntryAdapter):
-    """Adapter for Silva News Items (article, agenda) to get an atom/rss feed entry 
-    representation."""
-    grok.context(INewsItem)
+# class NewsItemFeedEntryAdapter(feedentry.DocumentFeedEntryAdapter):
+#     """Adapter for Silva News Items (article, agenda) to get an atom/rss feed entry 
+#     representation."""
+#     grok.context(INewsItem)
 
-    def html_description(self):
-        return self.version.get_intro()
+#     def html_description(self):
+#         return self.version.get_intro()
 
-    def date_published(self):
-        """ This field is used for ordering.
-        """
-        return self.version.display_datetime()
+#     def date_published(self):
+#         """ This field is used for ordering.
+#         """
+#         return self.version.display_datetime()
 
 
-class AgendaItemFeedEntryAdapter(NewsItemFeedEntryAdapter):
-    grok.context(IAgendaItem)
+# class AgendaItemFeedEntryAdapter(NewsItemFeedEntryAdapter):
+#     grok.context(IAgendaItem)
 
-    def location(self):
-        return self.version.get_location()
+#     def location(self):
+#         return self.version.get_location()
 
-    def start_datetime(self):
-        return self.version.get_start_datetime().isoformat()
+#     def start_datetime(self):
+#         return self.version.get_start_datetime().isoformat()
 
-    def end_datetime(self):
-        edt = self.version.get_end_datetime()
-        return (edt and edt.isoformat()) or edt
+#     def end_datetime(self):
+#         edt = self.version.get_end_datetime()
+#         return (edt and edt.isoformat()) or edt
